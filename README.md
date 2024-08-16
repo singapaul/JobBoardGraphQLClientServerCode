b13d08041dad5b30eb5920a359d970337a58104d
PaulHardman <paul.hardman@victorianplumbing.co.uk>
2024-08-16 16:13:02 +0100

    fixes

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This is a project used in the [GraphQL by Example](https://www.udemy.com/course/graphql-by-example/?referralCode=7ACEB04674F000BAC061) course.
 
-It uses Apollo Server with Express, and GraphQL-Request and Apollo Client as GraphQL clients. The application is used to explain queries, mutations, custom object types, authentication, etc.
+My personal notes available on notion here:
+
+https://www.notion.so/Graph-QL-Course-135f8455250f4b798637391e91e4e0db?pvs=4
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -2,31 +2,38 @@ import JobList from "../components/JobList";
 import { useJobs } from "../lib/graphql/hooks";
 import { useState } from "react";
 const JOBS_PER_PAGE = 5;
- 
 
 function HomePage() {
   const [currentPage, setCurrentPage] = useState(1);
-  const { jobs, loading, error } = useJobs(JOBS_PER_PAGE, (currentPage - 1) * JOBS_PER_PAGE);
+  const { jobs, loading, error } = useJobs(
+    JOBS_PER_PAGE,
+    (currentPage - 1) * JOBS_PER_PAGE
+  );
 
-
-
-  
   if (loading || error) {
     return <p>Loading...</p>;
   }
 
-  const totalPages = Math.ceil(jobs.totalCount/ JOBS_PER_PAGE)
+  const totalPages = Math.ceil(jobs.totalCount / JOBS_PER_PAGE);
 
   return (
     <div>
       <h1 className="title">Job Board</h1>
       <JobList jobs={jobs.items} />
       <div>
-        <button onClick={() => setCurrentPage(currentPage - 1)} disabled={currentPage === 1}>
+        <button
+          onClick={() => setCurrentPage(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
           previous
         </button>
         <span>{`${currentPage}/${totalPages}`}</span>
-        <button onClick={() => setCurrentPage(currentPage + 1)} disabled={currentPage === totalPages}>Next</button>
+        <button
+          onClick={() => setCurrentPage(currentPage + 1)}
+          disabled={currentPage === totalPages}
+        >
+          Next
+        </button>
       </div>
     </div>
   );
